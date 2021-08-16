4a0f6256132867d56a0ac8af0d496dfa2d24c668
cookie <Lsir1022@163.com>
2021-08-17 00:23:42 +0800

    新增spit模块

---
+++ tensquare_spit/src/test/java/com/example/tensquare_spit/TensquareSpitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tensquare_spit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TensquareSpitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
